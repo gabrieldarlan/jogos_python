aaf5c26a113c5a0abbcc262afd39dc3810cd65dd
gabrieldarlan <gabriel.darlan88@gmail.com>
2020-09-21 20:16:20 -0300

    cap 07 aula 01 completa

--- forca.py
+++ forca.py
@@ -1,9 +1,68 @@
+import random
+
+
 def jogar():
-    print("*********************************")
-    print("***Bem vindo ao jogo de Forca!***")
-    print("*********************************")
+
+    imprime_mensagem_abertura()
+    palavra_secreta = carrega_palavra_secreta()
+
+    letras_acertadas = inicializa_letras_acertadas(palavra_secreta)
+
+    enforcou = False
+    acertou = False
+    erros = 0
+
+    print(letras_acertadas)
+
+    while (not enforcou and not acertou):
+
+        chute = input("Qual é a letra? ")
+        chute = chute.strip().upper()
+
+        if(chute in palavra_secreta):
+            index = 0
+            for letra in palavra_secreta:
+                if (chute == letra):
+                    letras_acertadas[index] = letra
+                index += 1
+        else:
+            erros += 1
+
+        enforcou = erros == 6
+        acertou = "_" not in letras_acertadas
+        print(letras_acertadas)
+
+    if(acertou):
+        print('Você ganhou!')
+    else:
+        print('Você perdeu')
 
     print("Fim do jogo")
+
+
+def inicializa_letras_acertadas(palavra_secreta):
+    return ["_" for letra in palavra_secreta]
+
+
+def carrega_palavra_secreta():
+    arquivo = open("palavras.txt", "r")
+    palavras = []
+
+    for linha in arquivo:
+        linha = linha.strip()
+        palavras.append(linha)
+
+    arquivo.close()
+
+    numero = random.randrange(0, len(palavras))
+    palavra_secreta = palavras[numero].upper()
+    return palavra_secreta
+
+
+def imprime_mensagem_abertura():
+    print("********************************")
+    print("***Bem vindo ao jogo de Forca***")
+    print("********************************")
 
 
 if(__name__ == "__main__"):
